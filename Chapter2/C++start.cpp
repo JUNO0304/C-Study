51540ae82130a44669a4cf9fe4e91775a34b4aa4
JUNO0304 <danielyun076@gmail.com>
2026-01-16 16:41:24 +0900

    C++ 시작하기

---
+++ Chapter2/C++start.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "hellow World" << endl;
+    int x;
+    cout << "type in any number" << endl;
+    cin >> x;
+    cout << x << endl;
+
+    return 0;   
+}
